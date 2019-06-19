6ffcab7b7ef0f3bd7f66b55dae9462e29a3128ef
PiyushMichael <piyush.michael93@gmail.com>
2019-06-19 14:59:58 +0530

    to do child to parent again

--- src/video-item.js
+++ src/video-item.js
@@ -3,11 +3,16 @@
 import React from 'react';
 
 class VideoItem extends React.Component {
+	clicked = (e) => {
+		e.preventDefault();
+		console.log('child clicked');
+	}
+	
 	render(){
-		return <div className="video-item item">
+		return <div onClick={this.clicked} className="video-item item">
 			<img className="ui image" src={this.props.thumb}/>
 			<div className="content">
-				<a href={"https://www.youtube.com/watch?v="+this.props.link} className="header">{this.props.title}</a>
+				<a onClick={this.click} href={"https://www.youtube.com/watch?v="+this.props.link} className="header">{this.props.title}</a>
 				<p className="description">by {this.props.channel}</p>
 			</div>
 		</div>
